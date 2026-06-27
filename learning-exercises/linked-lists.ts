a975ac1bd98264619bf7cd4f36ae7cfbed10afa0
anousheh21 <anousheh1@gmail.com>
2026-06-27 10:52:17 +0100

    Initial commit

---
+++ learning-exercises/linked-lists.ts
@@ -0,0 +1,10 @@
+// Linked List Class
+
+
+// Function that traverses through the linked list
+
+
+// Function that adds a new node to the linked list
+
+
+// Function that deletes a node from the linked list
