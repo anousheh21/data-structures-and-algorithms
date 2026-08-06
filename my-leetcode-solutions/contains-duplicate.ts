017487cae470c46d6595dafaa69cbe3ea2437dea
anousheh21 <anousheh1@gmail.com>
2026-08-06 14:39:42 +0100

    Optimised contains duplicate solutions

--- my-leetcode-solutions/contains-duplicate.ts
+++ my-leetcode-solutions/contains-duplicate.ts
@@ -2,17 +2,11 @@
 
 // Brute Force Approach
 function containsDuplicate(nums: number[]): boolean {
-    // Sets check for duplicate values
-
-    // Convert the array into a set
     const numsSet = new Set<number>();
 
     for (let i = 0; i < nums.length; i++) {
         numsSet.add(nums[i]);
     }
-
-
-    // if numsSet.length == nums.length, return true, otherwise return false
 
     if(numsSet.size === nums.length) {
         return false
@@ -24,11 +18,57 @@ function containsDuplicate(nums: number[]): boolean {
 
 // Time Complexity: O(n)
 
-const testCases = [[1, 2, 3, 1], [], [1,1,1,1,1], [1,2,3,4]];
+// const testCases = [[1, 2, 3, 1], [], [1,1,1,1,1], [1,2,3,4]];
 
-for(let i = 0; i < testCases.length; i++) {
-    const output = containsDuplicate(testCases[i]);
-    console.log(`Output: ${output} for input: ${testCases[i]}`);
+// for(let i = 0; i < testCases.length; i++) {
+//     const output = containsDuplicate(testCases[i]);
+//     console.log(`Output: ${output} for input: ${testCases[i]}`);
+// }
+
+// Optimised Solution
+
+function containsDuplicate2(nums: number[]): boolean {
+    const numsSet = new Set<number>(nums);
+
+    if(numsSet.size === nums.length) {
+        return false
+    } else { 
+        return true
+    }
+};
+
+// Time Complexity: O(n)
+
+// const testCases2 = [[1, 2, 3, 1], [], [1,1,1,1,1], [1,2,3,4]];
+
+// for(let i = 0; i < testCases2.length; i++) {
+//     const output2 = containsDuplicate2(testCases2[i]);
+//     console.log(`Output: ${output2} for input: ${testCases2[i]}`);
+// }
+
+// Optimised Solution 2
+
+function containsDuplicate3(nums: number[]): boolean {
+    const setNums = new Set<number>();
+
+    for(let i = 0; i < nums.length; i++) {
+        if(setNums.has(nums[i])) {
+            return true;
+        } else {
+            setNums.add(nums[i]);
+        }
+    }
+
+    return false;
+};
+
+// Time Complexity: O(n)
+
+const testCases3 = [[1, 2, 3, 1], [], [1,1,1,1,1], [1,2,3,4]];
+
+for(let i = 0; i < testCases3.length; i++) {
+    const output3 = containsDuplicate3(testCases3[i]);
+    console.log(`Output: ${output3} for input: ${testCases3[i]}`);
 }
 
 export {}
