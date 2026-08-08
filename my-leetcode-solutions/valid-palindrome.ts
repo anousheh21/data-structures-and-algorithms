17fdc11f4b5a3ed08b2bf83d17803d3afa401a5f
anousheh21 <anousheh1@gmail.com>
2026-08-08 22:00:03 +0100

    Valid Palindrome solution

--- my-leetcode-solutions/valid-palindrome.ts
+++ my-leetcode-solutions/valid-palindrome.ts
@@ -1,8 +1,36 @@
 // My solution to 125 - Valid Palindrome
 
 function isPalindrome(s: string): boolean {
-    
+    // Convert to lowercase
+    const lower = s.toLowerCase();
+
+    // Remove all non-alphanumeric characters
+    const regex = /[a-z0-9]+/g;
+    const alphanumeric = lower.match(regex);
+    const joined = alphanumeric?.join("");
+
+    if (joined == null) {
+        return true;
+    }
+
+    // Use opposite end pointers to check if the remaining string is a palindrome
+    let left = 0;
+    let right = joined.length - 1;
+
+    while (left < right) {
+        if (joined[left] !== joined[right]) {
+            return false;
+        } else {
+            left++;
+            right--;
+        }
+    }
+
+    return true;
+
 };
+
+// Time Complexity: O(n)
 
 const testCases = [
     "A man, a plan, a canal: Panama",
