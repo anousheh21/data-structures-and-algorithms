450a12efc8cedb71c88d11af9aa6137644539436
anousheh21 <anousheh1@gmail.com>
2026-08-15 12:08:08 +0100

    solution for search insert position

--- my-leetcode-solutions/search-insert-position.ts
+++ my-leetcode-solutions/search-insert-position.ts
@@ -1,10 +1,27 @@
 // My solution to 35 - Search Insert Position
 
 function searchInsert(nums: number[], target: number): number {
-    
+    let mid = Math.floor(nums.length / 2);
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start <= end) {
+        if (nums[mid] === target) {
+            return mid;
+        } else if (target < nums[mid]) {
+            end = mid - 1;
+        } else if (target > nums[mid]) {
+            start = mid + 1;
+        }
+
+        const diff = end - start;
+        mid = end - Math.ceil(diff / 2);
+    }
+
+    return mid + 1;
 };
 
-// Time Complexity:
+// Time Complexity: O(log n)
 
 const numsTestCases = [[1, 3, 5, 6], [1, 3, 5, 6], [1, 3, 5, 6]];
 const targetTestCases = [5, 2, 7];
