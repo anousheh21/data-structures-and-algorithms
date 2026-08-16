760adc01ccb5b8badd9b3f1c127be56e341b12a3
anousheh21 <anousheh1@gmail.com>
2026-08-16 17:20:06 +0100

    working solution for search in rotated sorted array

--- my-leetcode-solutions/search-in-rotated-sorted-array.ts
+++ my-leetcode-solutions/search-in-rotated-sorted-array.ts
@@ -4,46 +4,54 @@ function search(nums: number[], target: number): number {
     let mid = Math.floor(nums.length / 2);
     let start = 0;
     let end = nums.length - 1;
-    let leftRotated = false;
 
     while (start <= end) {
-        if (nums[start] > nums[end]) {
-            leftRotated = true;
-        }
-
-        if (leftRotated === true) {
-            if(nums[mid] === target) {
-                return mid;
-            }  else if (target > nums[mid] && nums[mid] < nums[mid + 1]) {
-                start = mid + 1;
-            } else if (target > nums[mid] && nums[mid] > nums[mid + 1]) {
-                return -1;
-            } else if (target < nums[mid] && target >= nums[start]) {
-                end = mid - 1;
-            } else if (target < nums[mid] && target < nums[start]) {
-                start = mid + 1;
-
-                if (target > nums[start]) {
-                    return -1;
-                }
-            }
-
-        } else {
             if (nums[mid] === target) {
                 return mid;
-            } else if (target < nums[mid]) {
-                end = mid - 1;
-            } else if (target > nums[mid]) {
-                start = mid + 1;
+            } else {
+                let leftSorted = true;
+                let rightSorted = true;
+
+                if((nums[mid] - nums[start]) < 0) {
+                    leftSorted = false;
+                } else if ((nums[end] - nums[mid]) < 0) {
+                    rightSorted = false;
+                }
+
+                if(target < nums[mid]) {
+                    if(leftSorted && rightSorted) {
+                        end = mid - 1;
+                    } else if (leftSorted && !rightSorted) {
+                        if (target < nums[start]) {
+                            start = mid + 1;
+                        } else if (target > nums[start]) {
+                            end = mid - 1;
+                        } else if (target === nums[start]) {
+                            return start;
+                        }
+                    } else if (!leftSorted && rightSorted) {
+                        end = mid - 1;
+                    }
+                } else if (target > nums[mid]) {
+                    if(leftSorted && rightSorted) {
+                        start = mid + 1;
+                    } else if (leftSorted && !rightSorted) {
+                        start = mid + 1;
+                    } else if (!leftSorted && rightSorted) {
+                        if (target > nums[end]) {
+                            end = mid - 1;
+                        } else if (target < nums[end]) {
+                            start = mid + 1;
+                        } else if (target === nums[end]) {
+                            return end;
+                        }
+                    }
+                }
             }
-        }
+        
 
         const diff = end - start;
         mid = end - Math.ceil(diff / 2);
-        leftRotated = false;
-        // console.log(mid)
-        // console.log(start)
-        // console.log(end)
     }
 
     return -1;
@@ -51,8 +59,8 @@ function search(nums: number[], target: number): number {
 
 // Time Complexity: O(log n)
 
-const numsTestCases = [[4, 5, 6, 7, 0, 1, 2], [4, 5, 6, 7, 0, 1, 2], [1], [1], [5, 1, 3]];
-const targetTestCases = [0, 3, 0, 2, 5];
+const numsTestCases = [[4, 5, 6, 7, 0, 1, 2], [4, 5, 6, 7, 0, 1, 2], [1], [1], [5, 1, 3], [4,5,6,7,0,1,2], [3,5,1]];
+const targetTestCases = [0, 3, 0, 2, 5, 1, 3];
 
 for (let i = 0; i < numsTestCases.length; i++) {
     const output = search(numsTestCases[i], targetTestCases[i]);
