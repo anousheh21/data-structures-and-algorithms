faf3500f2d70c33826f9a51e3de372870b7429aa
anousheh21 <anousheh1@gmail.com>
2026-08-16 09:38:53 +0100

    nearly there for merge intervals but confused about a particular edge case

--- my-leetcode-solutions/merge-intervals.ts
+++ my-leetcode-solutions/merge-intervals.ts
@@ -5,27 +5,71 @@ function merge(intervals: number[][]): number[][] {
     intervals.sort((a, b) => a[0] - b[0]);
     const mergedIntervals: number[][] = [];
 
-    
+    let currentStart = intervals[0][0];
+    let currentEnd = intervals[0][1];
+    let currentInterval = intervals[0];
+    let last2Merged: boolean = true;
+
     for(let i = 0; i < intervals.length - 1; i++) {
-        let currentEnd = intervals[i][1];
+        // let currentEnd = intervals[i][1];
         let nextStart = intervals[i+1][0];
 
         if (currentEnd >= nextStart) {
-            mergedIntervals.push([intervals[i][0], intervals[i+1][1]]);
+            // mergedIntervals.push([intervals[i][0], intervals[i+1][1]]);
+            // update current end and current interval
+
+            // currentEnd = intervals[i+1][1]
+            currentEnd = intervals[i+1][1] > intervals[i][1] ? intervals[i+1][1] : intervals[i][1];
+            currentInterval = [currentStart, currentEnd];
+            last2Merged = true;
+
         } else {
-            mergedIntervals.push(intervals[i]);
+            mergedIntervals.push(currentInterval);
+            currentStart = intervals[i+1][0];
+            currentEnd = intervals[i+1][1];
+            currentInterval = [currentStart, currentEnd];
+            // mergedIntervals.push(currentInterval)
+            last2Merged = false;
         }
     }
+
+    if (last2Merged === false) {
+             mergedIntervals.push(currentInterval);
+        currentStart = intervals[intervals.length - 1][0];
+        currentEnd = intervals[intervals.length - 1][1];
+        currentInterval = [currentStart, currentEnd];
+    }
+
+    const lastInterval = intervals[intervals.length - 1];
+
+    if(last2Merged === true) {
+        mergedIntervals.push(currentInterval);
+    } else if (last2Merged === false && lastInterval[0] > currentInterval[1]) {
+        mergedIntervals.push(lastInterval);
+    } else if (last2Merged === false && lastInterval[0] > currentInterval[1]) {
+        currentInterval = [lastInterval[0], currentInterval[1]];
+        mergedIntervals.push(currentInterval)
+    }
+    
+
+    // mergedIntervals.push(currentInterval);
+
+    // const lastInterval = intervals[intervals.length - 1];
+
+
+    
 
     return mergedIntervals;
 };
 
-// Time Complexity:
+// Time Complexity: O(n)
 
 const testCases = [
     [[1, 3], [2, 6], [8, 10], [15, 18]],
     [[1, 4], [4, 5]],
     [[4, 7], [1, 4]],
+    [[1,4],[2,3]],
+    [[2,3],[4,5],[6,7],[8,9],[1,10]]
 ];
 
 for (let i = 0; i < testCases.length; i++) {
@@ -34,3 +78,15 @@ for (let i = 0; i < testCases.length; i++) {
 }
 
 export {};
+
+
+// currentStart = 1; currentEnd = 4; currentInterval = [1, 4]
+
+
+
+// currentStart = 1; currentEnd = 3; currentInterval = [1, 3]; nextStart = 2
+// currentEnd = 6; currentInteval = [1, 6]
+
+// nextStart = 8; mergedIntervals = [[1, 6]]; currentStart = 8; currentEnd = 10
+
+// nextStart = 15; 
