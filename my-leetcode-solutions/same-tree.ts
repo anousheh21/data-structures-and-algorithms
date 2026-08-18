68388004592530c19858bc6f14bc7ed33f15a67d
anousheh21 <anousheh1@gmail.com>
2026-08-18 22:16:07 +0100

    created file for same tree problem

---
+++ my-leetcode-solutions/same-tree.ts
@@ -0,0 +1,52 @@
+// My solution to 100 - Same Tree
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    
+};
+
+// Time Complexity:
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+const pTestCases = [
+    new TreeNode(1, new TreeNode(2), new TreeNode(3)),
+    new TreeNode(1, new TreeNode(2)),
+    new TreeNode(1, new TreeNode(2), new TreeNode(1)),
+];
+
+const qTestCases = [
+    new TreeNode(1, new TreeNode(2), new TreeNode(3)),
+    new TreeNode(1, null, new TreeNode(2)),
+    new TreeNode(1, new TreeNode(1), new TreeNode(2)),
+];
+
+for (let i = 0; i < pTestCases.length; i++) {
+    const output = isSameTree(pTestCases[i], qTestCases[i]);
+    console.log(`Output: ${output} for p as ${JSON.stringify(pTestCases[i])} and q as ${JSON.stringify(qTestCases[i])}`);
+}
+
+export {};
