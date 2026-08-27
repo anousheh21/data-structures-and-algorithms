5a48380351b3703a027fec2caa4027bcc71e8dce
anousheh21 <anousheh1@gmail.com>
2026-08-27 14:30:42 +0100

    solution to validate binary search tree

--- my-leetcode-solutions/validate-binary-search-tree.ts
+++ my-leetcode-solutions/validate-binary-search-tree.ts
@@ -15,8 +15,43 @@
  */
 
 function isValidBST(root: TreeNode | null): boolean {
+    const orderedArray: number[] = [];
+
+    const helper = (root: TreeNode | null, orderedArray: number[]): number[] => {
+          if(root === null) {
+            return [];
+        }
+
+        helper(root.left, orderedArray);
+        
+        orderedArray.push(root.val)
+        // console.log(orderedArray)
+        helper(root.right, orderedArray);
+
+        return orderedArray;
+    }
     
+   const output = helper(root, orderedArray);
+
+   // check every value in output is unique
+
+   const outputSet = new Set<number>(output);
+   if (output.length !== outputSet.size) {
+    return false;
+   }
+
+   const sortedOutput = [...output].sort((a, b) => a - b)
+
+    const equal = output.every((value, index) => value === sortedOutput[index])
+
+    if (equal) {
+        return true;
+    } else {
+        return false;
+    }
+
 };
+
 
 // Time Complexity:
 
@@ -35,6 +70,7 @@ class TreeNode {
 const rootTestCases = [
     new TreeNode(2, new TreeNode(1), new TreeNode(3)),
     new TreeNode(5, new TreeNode(1), new TreeNode(4, new TreeNode(3), new TreeNode(6))),
+    new TreeNode(5, new TreeNode(4), new TreeNode(6, new TreeNode(3), new TreeNode(7))),
 ];
 
 for (let i = 0; i < rootTestCases.length; i++) {
@@ -43,3 +79,10 @@ for (let i = 0; i < rootTestCases.length; i++) {
 }
 
 export {};
+
+// depth first search
+// for each node, check that current > left and current < right
+// if that condition breaks, return false and don't bother continuing
+// if you get to the end and it's still true, return true
+
+// But the problem is, you need to ensure that there are no nodes in left subtreees that are bigger than any node in a level above, and no nodes in right subtrees that are smaller than any level above.
