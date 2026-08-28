1945c682dbe298e6f561a3baf61ef85e92b8a284
anousheh21 <anousheh1@gmail.com>
2026-08-28 10:45:52 +0100

    added file for last stone weight

---
+++ my-leetcode-solutions/last-stone-weight.ts
@@ -0,0 +1,16 @@
+// My solution to 1046 - Last Stone Weight
+
+function lastStoneWeight(stones: number[]): number {
+    
+};
+
+// Time Complexity:
+
+const testCases = [[2, 7, 4, 1, 8, 1], [1]];
+
+for (let i = 0; i < testCases.length; i++) {
+    const output = lastStoneWeight(testCases[i]);
+    console.log(`Output: ${output} for input: ${testCases[i]}`);
+}
+
+export {};
