76f49fb63ad2eb98f0bf7861fd9f2b5df0c0fb7e
anousheh21 <anousheh1@gmail.com>
2026-08-29 12:35:57 +0100

    started thinking about potential solutions to flood fill

--- my-leetcode-solutions/flood-fill.ts
+++ my-leetcode-solutions/flood-fill.ts
@@ -20,3 +20,8 @@ for (let i = 0; i < imageTestCases.length; i++) {
 }
 
 export {};
+
+// To find neighbours, you find the position, so image[i][j], and then it's neighbours are image[i+1][j] and image [i][j+1]. But you need to check that i+1 < m and j+i < n
+
+// Start at image[sr][sc]. Set originalColour to the starting colour of that pixel.
+// DFS: for each node that has the same colour as originalColour, change it's colour to colour. As soon as you reach a node whose colour is not originalColor, that's a base case, so it will go back up the call stack. So for each bit, you check the two neighbours of the current node. 
