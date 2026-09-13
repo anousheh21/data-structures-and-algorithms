71ac470e357844fddfd54b31f0e1551df10f1e76
anousheh21 <anousheh1@gmail.com>
2026-09-13 18:06:24 +0100

    initial solution for best time to buy and sell stock ii, but it fails for some edge cases

--- my-leetcode-solutions/best-time-to-buy-and-sell-stock-ii.ts
+++ my-leetcode-solutions/best-time-to-buy-and-sell-stock-ii.ts
@@ -1,8 +1,43 @@
 // My solution to 122 - Best Time to Buy and Sell Stock II
 
 function maxProfit(prices: number[]): number {
-    
+    let maxProfit = 0;
+    let currentHeldShareIndex = 0;
+    let holdingShare = false;
+
+    for(let i = 1; i < prices.length; i++) {
+        if (!holdingShare) {
+            // check decreasing values. as soon as you find an increasing value, hold the share before the increasing value
+            if(prices[i] > prices[i - 1]) {
+                currentHeldShareIndex = i - 1;
+                holdingShare = true;
+
+                if(prices.length === 2) {
+                    maxProfit = prices[i] - prices[i - 1];
+                }
+            }
+
+            
+        } else if (holdingShare) {
+            // check increasing values. as soon as you find a decreasing value, sell the share before the decreasing value
+            if(prices[i] < prices[i - 1]) {
+                maxProfit = maxProfit + (prices[i - 1] - prices[currentHeldShareIndex]);
+                holdingShare = false;
+            } else if (i === prices.length - 1) {
+                maxProfit = maxProfit + (prices[i] - prices[currentHeldShareIndex]);
+            }
+        }
+    }
+
+    if (holdingShare) {
+        
+    }
+
+    return maxProfit;
 };
+
+// if you are holding a share, you keep holding that while values increase. when you find a decreasing value, you sell at the one before that.
+// if you are not holding a share, you check decreasing values, and as soon as it increases, you hold the share before it increases
 
 // Time Complexity:
 
@@ -10,6 +45,8 @@ const testCases = [
     [7, 1, 5, 3, 6, 4],
     [1, 2, 3, 4, 5],
     [7, 6, 4, 3, 1],
+    [1, 2],
+    [2, 1, 4],
 ];
 
 for (let i = 0; i < testCases.length; i++) {
