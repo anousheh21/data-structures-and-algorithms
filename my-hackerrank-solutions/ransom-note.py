9eb64b2b175b6d3f268dc718db0f3c2a638ac925
anousheh21 <anousheh1@gmail.com>
2026-09-15 20:55:42 +0100

    completed ransom note hackerrank challenge

--- my-hackerrank-solutions/ransom-note.py
+++ my-hackerrank-solutions/ransom-note.py
@@ -3,7 +3,26 @@
 
 
 def checkMagazine(magazine: list[str], note: list[str]) -> None:
-    pass
+    # dictionary for magazine. key is the word, value is the number of times it appears
+
+    dict = {}
+
+    for word in magazine:
+        if word not in dict:
+            dict[word] = 1
+        else:
+            dict[word] = dict[word] + 1
+
+    for word in note:
+        if word not in dict:
+            print("No")
+            return
+        else:
+            dict[word] = dict[word] - 1
+            if dict[word] == 0:
+                del dict[word]
+
+    print("Yes")
 
 
 test_cases = [
@@ -19,12 +38,15 @@ test_cases = [
         ["ive", "got", "a", "lovely", "bunch", "of", "coconuts"],
         ["ive", "got", "some", "coconuts"],
     ),
+    (
+        ["beans", "beans", "beans"],
+        ["no", "beans"]
+    ),
 ]
 
 
 for magazine_words, note_words in test_cases:
     print(f"Magazine: {magazine_words}")
     print(f"Note: {note_words}")
-    print("Output: ", end="")
-    checkMagazine(magazine_words, note_words)
+    print(f"Output: {checkMagazine(magazine_words, note_words)}")
     print()
