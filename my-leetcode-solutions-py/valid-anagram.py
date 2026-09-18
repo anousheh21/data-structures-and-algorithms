cd1fbf41a5fa045e06471c2871d80c6c1fef343f
anousheh21 <anousheh1@gmail.com>
2026-09-18 21:30:22 +0100

    valid anagram

--- my-leetcode-solutions-py/valid-anagram.py
+++ my-leetcode-solutions-py/valid-anagram.py
@@ -2,13 +2,45 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        pass
+        """
+        length is the same
+
+        create a map of both. if the maps are the same, they are the same. however to prevent having to check both ways, you can check one way and check the length as well
+        """
+
+        if len(s) != len(t):
+            return False
+
+        # convert both to a map
+        sMap = self.toMap(s)
+        tMap = self.toMap(t)
+
+        for key, val in sMap.items():
+            if key not in tMap:
+                return False
+
+            if sMap[key] != tMap[key]:
+                return False
+
+    
+
+        return True
+
+    
+    def toMap(self, string: str):
+        map = {}
+
+        for char in string:
+            map[char] = map.get(char, 0) + 1
+
+        return map
+
 
 
 # Time Complexity:
 
-s_test_cases = ["anagram", "rat"]
-t_test_cases = ["nagaram", "car"]
+s_test_cases = ["anagram", "rat", "anagramm", "aacc"]
+t_test_cases = ["nagaram", "car", "managram", "ccac"]
 
 solution = Solution()
 
