e9404c0bc8f17d07b2178c8dfa32f55258455fff
anousheh21 <anousheh1@gmail.com>
2026-09-19 22:20:08 +0100

    solved maximum average subarray i

--- my-leetcode-solutions-py/maximum-average-subarray-i.py
+++ my-leetcode-solutions-py/maximum-average-subarray-i.py
@@ -2,7 +2,35 @@
 
 class Solution:
     def findMaxAverage(self, nums: list[int], k: int) -> float:
-        
+        """
+        Sliding window of length k.
+
+        Can't sort it bc you need it to be contiguous. 
+        You go through the array and calculate every average BUT you don't recalculate some bits. 
+        """
+       
+
+        # calculate initial average
+        initialAverage = sum(nums[:k]) / k
+        averages = [initialAverage]
+        maxAverage = initialAverage
+
+        for i in range(1, len(nums) - k + 1):
+            # print(nums[i:k+i])
+
+            # calculate current average, and if that is greater than maxAveage, assign it
+            currentAverage = ((averages[i-1] * k) - nums[i-1] + nums[k-1+i]) / k
+            averages.append(currentAverage)
+
+            if currentAverage > maxAverage:
+                maxAverage = currentAverage
+
+        return maxAverage
+
+            
+
+
+
 
 
 # Time Complexity:
@@ -10,9 +38,10 @@ class Solution:
 nums_test_cases = [
     [1, 12, -5, -6, 50, 3],
     [5],
+    [3,3,4,3,0]
 ]
 
-k_test_cases = [4, 1]
+k_test_cases = [4, 1, 3]
 
 solution = Solution()
 
