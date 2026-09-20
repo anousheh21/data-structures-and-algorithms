23c7a5702b976f481e736ac47ea6796156ee827c
anousheh21 <anousheh1@gmail.com>
2026-09-20 17:34:40 +0100

    non-optimal approach to 3

--- my-leetcode-solutions-py/longest-substring-without-repeating-characters.py
+++ my-leetcode-solutions-py/longest-substring-without-repeating-characters.py
@@ -2,12 +2,54 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+        maxLen = 1
+        startPointer = 0
+        endPointer = 0
+
+        if len(s) == 0:
+            maxLen = 0
+
+        while endPointer < len(s):
+            if startPointer == endPointer:
+                endPointer += 1
+
+                if endPointer >= len(s):
+                    break
+
+                if s[startPointer] != s[endPointer] and startPointer == 0 and endPointer == 1:
+                    # print(f"{startPointer} and {endPointer}")
+                    maxLen = 2
+                    
+
+
+            elif s[startPointer] == s[endPointer]:
+                startPointer += 1
+
+            elif s[endPointer] in s[startPointer:endPointer]:
+                # print(s[startPointer:endPointer+1])
+                startPointer += 1 
+
+            else:
+                difference = endPointer - startPointer + 1
+                
+                if difference > maxLen:
+                    maxLen = difference
+                    # print({s[startPointer:endPointer+1]})
+
+                endPointer += 1
+
+                if endPointer >= len(s):
+                    break
+
+        return maxLen
+       
+
+
 
 
 # Time Complexity:
 
-s_test_cases = ["abcabcbb", "bbbbb", "pwwkew"]
+s_test_cases = ["abcabcbb", "bbbbb", "pwwkew", "<Dx", "S", "mmq", "mq", "kbdbl"]
 
 solution = Solution()
 
