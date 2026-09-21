7663fe2def53ea3bf59bc0a1b5a341abdde4124e
anousheh21 <anousheh1@gmail.com>
2026-09-21 23:25:48 +0100

    search insert position

--- my-leetcode-solutions-py/search-insert-position.py
+++ my-leetcode-solutions-py/search-insert-position.py
@@ -2,7 +2,27 @@
 
 class Solution:
     def searchInsert(self, nums: list[int], target: int) -> int:
-        pass
+        start = 0
+        end = len(nums) - 1
+        mid = end // 2
+
+        while start <= end:
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            elif nums[mid] > target:
+                end = mid - 1
+
+            difference = (end - start) // 2
+            mid = start + difference
+
+        if target < nums[mid]:
+            if mid == -1:
+                return 0
+            return mid - 1
+        else:
+            return mid + 1
 
 
 # Time Complexity:
