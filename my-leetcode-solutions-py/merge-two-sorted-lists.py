d3fd1e102b3aaa3b172d9678e8d0d819f5fdaca2
anousheh21 <anousheh1@gmail.com>
2026-09-21 22:10:43 +0100

    merge 2 sorted linked lists

--- my-leetcode-solutions-py/merge-two-sorted-lists.py
+++ my-leetcode-solutions-py/merge-two-sorted-lists.py
@@ -11,10 +11,64 @@ class Solution:
     def mergeTwoLists(
         self, list1: ListNode | None, list2: ListNode | None
     ) -> ListNode | None:
-        pass
+
+        if list1 == None and list2 == None:
+            return None
+        elif list1 == None:
+            return list2
+        elif list2 == None:
+            return list1
+
+        head = list1
+        if list2.val < list1.val:
+            head = list2
+            list2 = list2.next
+        else:
+            list1 = list1.next
+
+        mainCurrent = head
+        # mainCurrentNext = mainCurrent.next if mainCurrent is not None else None
+
+        list1Next = list1.next if list1 is not None else None
+        list2Next = list2.next if list2 is not None else None
+
+        print(mainCurrent.val)
+
+        while list1 is not None and list2 is not None:
+            print(f"{list1.val}, {list2.val}")
+            if list1.val < list2.val:
+                mainCurrent.next = list1
+                list1 = list1Next
+                list1Next = list1.next if list1 is not None else None
+            else:
+                mainCurrent.next = list2
+                list2 = list2Next
+                list2Next = list2.next if list2 is not None else None
+
+            mainCurrent = mainCurrent.next
+
+
+        if list1 is not None:
+            mainCurrent.next = list1
+        elif list2 is not None:
+            mainCurrent.next = list2
+
+        # connect end to remainder of non-finished rode
+        return head
 
 
 # Time Complexity:
+
+def linked_list_to_list(head):
+    values = []
+
+    current = head
+    while current is not None:
+        values.append(current.val)
+        current = current.next
+
+    return values
+
 
 list1_test_cases = [
     [1, 2, 4],
@@ -40,7 +94,9 @@ for i in range(len(list1_test_cases)):
         list2 = ListNode(value, list2)
 
     output = solution.mergeTwoLists(list1, list2)
+
     print(
-        f"Output: {output!r} for list1 as {list1_test_cases[i]!r} "
+        f"Output: {linked_list_to_list(output)!r} "
+        f"for list1 as {list1_test_cases[i]!r} "
         f"and list2 as {list2_test_cases[i]!r}"
     )
